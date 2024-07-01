37d17099e3bef414348c55da42f30750b6acb71e
Ryan Duer <ryan@allmaddesigns.com>
2024-07-01 13:35:51 -0700

    adds jokes

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -15,10 +15,27 @@ const Heading = (props: Heading) => {
 	const { headingChildren, variant, id, gutterBottom } = props;
 	const theme = useTheme();
 	const { palette } = theme;
+	const getJokes = async () => {
+		const results = await fetch(`https://official-joke-api.appspot.com/random_joke`, {
+			method: 'GET'
+		}).then((response) =>
+			response.json()
+		)
+		const punchline = `${results.punchline}`
+		const setup = `${results.setup}`
+		const triggerSetup = () => {
+			setTimeout(() => {
+				return alert(punchline)
+			}, 1500)
+			return alert(setup)
+		}
+		return triggerSetup()
+	}
 	React.useEffect(() => {
 		init("cf7c5d84")
 		window.CommandBar.boot('12345')
 		window.CommandBar.trackEvent('page-view', {})
+		window.CommandBar.addCallback('makeMeLaugh', getJokes)
 	}, [])
 	
 	return (
